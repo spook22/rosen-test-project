c047412d69bb5f1c4adf8be7cd42876d45aeebbf
rosenspasov@gmail.com <rosenspasov@gmail.com@da6d4326-de41-0410-86b8-b1320e0b3720>
2013-08-31 21:32:48 +0000

    Added PublishOperation enum

--- eda/nerv-jms/src/main/java/com/softwareag/eda/nerv/publish/JmsDefaultRouteCreator.java
+++ eda/nerv-jms/src/main/java/com/softwareag/eda/nerv/publish/JmsDefaultRouteCreator.java
@@ -8,34 +8,35 @@ import org.apache.camel.Route;
 import com.softwareag.eda.nerv.event.Event;
 
 public class JmsDefaultRouteCreator implements EventPublishListener {
-	
+
 	private final Map<String, Route> routesCache = new HashMap<String, Route>();
 
 	@Override
-	public void prePublish(Event event) {
+	public void onPublish(PublishOperation operation, Event event) {
 		/*
-		 * 1. Using JMX check if route exists for the given ID deducted from the event type.
-		 * 2. Use cache to optimize performance and not go to JMX every time.
-		 * 3. If the route does not exist - create it.
-		 * 4. Decide when/where to register the default JMS component.
+		 * 1. Using JMX check if route exists for the given ID deducted from the
+		 * event type. 2. Use cache to optimize performance and not go to JMX
+		 * every time. 3. If the route does not exist - create it. 4. Decide
+		 * when/where to register the default JMS component.
 		 */
-		Route route = routesCache.get(event.getType());
-		if (route == null) {
-			route = checkUsingJmx(event.getType());
+		switch (operation) {
+		case PRE_PUBLISH:
+			Route route = routesCache.get(event.getType());
 			if (route == null) {
-				createRoute(event.getType());
+				route = checkUsingJmx(event.getType());
+				if (route == null) {
+					createRoute(event.getType());
+				}
 			}
+			break;
+		default:
+			break;
 		}
-	}
-
-	@Override
-	public void postPublish(Event event) {
-		// Nothing to do here.
 	}
 
 	private void createRoute(String type) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	private Route checkUsingJmx(String type) {
