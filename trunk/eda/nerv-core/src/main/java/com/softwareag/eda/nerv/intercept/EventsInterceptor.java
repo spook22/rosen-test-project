934c1a124578b4324dc9c9c77ff9d45554001449
rosenspasov@gmail.com <rosenspasov@gmail.com@da6d4326-de41-0410-86b8-b1320e0b3720>
2013-12-15 20:04:40 +0000

    First step, moving from Spring DSL to Java DSL.

--- trunk/eda/nerv-core/src/main/java/com/softwareag/eda/nerv/intercept/EventsInterceptor.java
+++ trunk/eda/nerv-core/src/main/java/com/softwareag/eda/nerv/intercept/EventsInterceptor.java
@@ -5,16 +5,18 @@ import java.util.Collection;
 
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
-
-import com.softwareag.eda.nerv.NERV;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class EventsInterceptor implements Processor {
+
+	private static final Logger logger = LoggerFactory.getLogger(EventsInterceptor.class);
 
 	private static EventsInterceptor instance;
 
 	public static EventsInterceptor instance() {
 		if (instance == null) {
-			synchronized (NERV.class) {
+			synchronized (EventsInterceptor.class) {
 				if (instance == null) {
 					instance = new EventsInterceptor();
 				}
@@ -25,20 +27,25 @@ public class EventsInterceptor implements Processor {
 
 	private final Collection<Processor> interceptors = new ArrayList<>();
 
-	public EventsInterceptor() {
+	private EventsInterceptor() {
 		super();
 	}
 
 	public void registerInterceptor(Processor interceptor) {
+		logger.info("Added events interceptor.");
 		interceptors.add(interceptor);
 	}
 
 	public boolean unregisterInterceptor(Processor interceptor) {
+		logger.info("Removed events interceptor.");
 		return interceptors.remove(interceptor);
 	}
 
 	@Override
 	public void process(Exchange exchange) throws Exception {
+		if (logger.isDebugEnabled()) {
+			logger.info("Intercepted event: " + exchange.getIn());
+		}
 		for (Processor interceptor : interceptors) {
 			interceptor.process(exchange);
 		}
