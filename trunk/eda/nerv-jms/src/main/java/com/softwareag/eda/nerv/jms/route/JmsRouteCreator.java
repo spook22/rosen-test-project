e068660c418102469f6406caa69f62eb73dd170d
rosenspasov@gmail.com <rosenspasov@gmail.com@da6d4326-de41-0410-86b8-b1320e0b3720>
2014-02-05 17:14:19 +0000

    Started using the routesCache.

--- trunk/eda/nerv-jms/src/main/java/com/softwareag/eda/nerv/jms/route/JmsRouteCreator.java
+++ trunk/eda/nerv-jms/src/main/java/com/softwareag/eda/nerv/jms/route/JmsRouteCreator.java
@@ -1,9 +1,8 @@
 package com.softwareag.eda.nerv.jms.route;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.HashSet;
+import java.util.Set;
 
-import org.apache.camel.Route;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -18,7 +17,7 @@ public class JmsRouteCreator implements RouteCreator {
 
 	private static final Logger logger = LoggerFactory.getLogger(JmsRouteCreator.class);
 
-	private final Map<String, Route> routesCache = new HashMap<String, Route>();
+	private final Set<String> routesCache = new HashSet<String>();
 
 	private final JmxHelper jmxHelper = new JmxHelper();
 
@@ -32,19 +31,18 @@ public class JmsRouteCreator implements RouteCreator {
 	}
 
 	/*
-	 * 1. Using JMX check if route exists for the given ID deducted from the
-	 * event type. 2. Use cache to optimize performance and not go to JMX every
-	 * time. 3. If the route does not exist - create it. 4. Decide when/where to
-	 * register the default JMS component.
+	 * 1. Using JMX check if route exists for the given ID deducted from the event type.
+	 * 
+	 * 2. Use cache to optimize performance and not go to JMX every time.
+	 * 
+	 * 3. If the route does not exist - create it. 4. Decide when/where to register the default JMS component.
 	 */
 	@Override
 	public void createRoute(String from, String eventType) throws NERVException {
-		Route route = routesCache.get(from);
-		if (route == null && !jmxHelper.routeExists("jmsRouteFor:" + from)) {
+		if (!routesCache.contains(from) && !jmxHelper.routeExists("jmsRouteFor:" + from)) {
 			String jmsChannel = jmsChannelProvider.channel(eventType);
 			if (logger.isInfoEnabled()) {
-				logger.info(String.format("Creating JMS route for event type %s from channel %s to JMS channel %s.",
-						eventType, from, jmsChannel));
+				logger.info(String.format("Creating JMS route for event type %s from channel %s to JMS channel %s.", eventType, from, jmsChannel));
 			}
 			JmsRouteBuilder builder = new JmsRouteBuilder(from, jmsChannel);
 			try {
@@ -53,6 +51,7 @@ public class JmsRouteCreator implements RouteCreator {
 				throw new NERVException("Cannot add routes to context.", e);
 			}
 		}
+		routesCache.add(from);
 	}
 
 }
