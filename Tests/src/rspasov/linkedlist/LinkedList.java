dc759f0b22784bf2edddb0d56aa7cce5201592de
Rosen Spasov <rosenspasov@gmail.com>
2017-08-05 14:25:39 +0300

    Moving test package under rspasov

---
+++ Tests/src/rspasov/linkedlist/LinkedList.java
@@ -0,0 +1,124 @@
+package rspasov.linkedlist;
+
+public class LinkedList<T> {
+
+	private Node<T> root;
+
+	private Node<T> last;
+
+	public LinkedList() {
+	}
+
+	public LinkedList(Node<T> root) {
+		this.root = root;
+		this.last = last();
+	}
+
+	@SafeVarargs
+	public LinkedList(T... data) {
+		if (data != null) {
+			for (T datum : data) {
+				add(new Node<T>(datum));
+			}
+		}
+	}
+
+	private Node<T> last() {
+		Node<T> node = root;
+		if (node != null) {
+			while (node.getNext() != null) {
+				node = node.getNext();
+			}
+		}
+		return node;
+	}
+
+	public Node<T> getRoot() {
+		return root;
+	}
+
+	public void add(T value) {
+		add(new Node<T>(value));
+	}
+
+	public void add(Node<T> node) {
+		if (root == null) {
+			root = node;
+		} else {
+			last.setNext(node);
+		}
+		last = node;
+	}
+
+	public void reverse() {
+		Node<T> prev = null;
+		Node<T> curr = root;
+		while (curr != null) {
+			Node<T> next = curr.getNext();
+			curr.setNext(prev);
+			prev = curr;
+			curr = next;
+		}
+		root = prev;
+	}
+
+	// This will lead to stack overflow if the size of the list is huge.
+	public void recursiveReverse() {
+		if (root != null) {
+			LinkedList<T> temp = new LinkedList<T>();
+			revAdd(temp, root);
+			root = temp.root;
+		}
+	}
+
+	private void revAdd(LinkedList<T> list, Node<T> node) {
+		Node<T> next = node.getNext();
+		if (next != null) {
+			revAdd(list, next);
+		}
+		list.add(node);
+		node.setNext(null);
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder("[");
+		if (root != null) {
+			builder.append(root);
+		}
+		builder.append("]");
+		return builder.toString();
+	}
+
+	public void sort() {
+		boolean swapped = (root != null);
+		while (swapped) {
+			swapped = false;
+			Node<T> prev = null;
+			Node<T> curr = root;
+			Node<T> next = curr.getNext();
+			while (next != null) {
+				if (curr.getData() instanceof Comparable<?> && next.getData() instanceof Comparable<?>) {
+					@SuppressWarnings("unchecked")
+					Comparable<T> currData = (Comparable<T>) curr.getData();
+					if (currData.compareTo(next.getData()) > 0) {
+						swapped = true;
+						if (prev == null) {
+							root = next;
+						} else {
+							prev.setNext(next);
+						}
+						curr.setNext(next.getNext());
+						next.setNext(curr);
+					}
+				}
+				if (curr.getData().toString().compareTo(next.getData().toString()) > 0) {
+				}
+				prev = curr;
+				curr = next;
+				next = next.getNext();
+			}
+		}
+	}
+
+}
---
+++ Tests/src/rspasov/linkedlist/LinkedListTest.java
@@ -0,0 +1,110 @@
+package rspasov.linkedlist;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+import org.junit.Test;
+
+public class LinkedListTest {
+
+	@Test
+	public void testLinkedListNullArray() {
+		LinkedList<Integer> list = new LinkedList<Integer>((Integer[]) null);
+		assertNull(list.getRoot());
+	}
+
+	@Test
+	public void testLinkedListIntArray() {
+		LinkedList<Integer> list = new LinkedList<Integer>(1, 2, 3, 4, 5);
+		Node<Integer> node = list.getRoot();
+		assertNotNull(node);
+		Integer value = 1;
+		while (node != null) {
+			assertEquals(value++, node.getData());
+			node = node.getNext();
+		}
+	}
+
+	@Test
+	public void testReverse() {
+		LinkedList<Integer> list = new LinkedList<Integer>(1, 2, 3, 4, 5);
+		list.reverse();
+		Node<Integer> node = list.getRoot();
+		assertNotNull(node);
+		Integer value = 5;
+		while (node != null) {
+			assertEquals(value--, node.getData());
+			node = node.getNext();
+		}
+
+		list = new LinkedList<Integer>();
+		list.reverse();
+	}
+
+	@Test
+	public void testReverseEmptyList() {
+		LinkedList<Integer> list = new LinkedList<Integer>();
+		assertNull(list.getRoot());
+		list.reverse();
+		assertNull(list.getRoot());
+	}
+
+	@Test
+	public void testReverseHugeList() {
+		LinkedList<Integer> list = new LinkedList<Integer>();
+		int max = 1000000;
+		int index = 0;
+		while (index < max) {
+			list.add(index++);
+		}
+		list.reverse();
+		Node<Integer> node = list.getRoot();
+		assertNotNull(node);
+		Integer value = max - 1;
+		while (node != null) {
+			assertEquals(value--, node.getData());
+			node = node.getNext();
+		}
+	}
+
+	@Test
+	public void testRecursiveReverse() {
+		LinkedList<Integer> list = new LinkedList<Integer>();
+		int max = 1000;
+		int index = 0;
+		while (index < max) {
+			list.add(index++);
+		}
+		list.reverse();
+		Node<Integer> node = list.getRoot();
+		assertNotNull(node);
+		Integer value = max - 1;
+		while (node != null) {
+			assertEquals(value--, node.getData());
+			node = node.getNext();
+		}
+	}
+
+	@Test
+	public void testSort() {
+		LinkedList<Integer> list = new LinkedList<Integer>();
+		int max = 100;
+		int index = 0;
+		while (index < max) {
+			list.add(index++);
+		}
+		list.reverse();
+		System.out.println(list);
+		list.sort();
+		System.out.println(list);
+		Node<Integer> node = list.getRoot();
+		assertNotNull(node);
+		Integer value = 0;
+		while (node != null) {
+			assertEquals(value++, node.getData());
+			node = node.getNext();
+		}
+
+	}
+}
