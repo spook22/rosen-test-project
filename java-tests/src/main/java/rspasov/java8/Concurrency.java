093008b7acbed6f93043a23925e2be2444ab10d1
rspasov <rspasov@vmware.com>
2019-02-05 17:56:29 +0200

    Some more examples with Future, CompletableFuture, Supplier, etc.

--- java-tests/src/main/java/rspasov/java8/Concurrency.java
+++ java-tests/src/main/java/rspasov/java8/Concurrency.java
@@ -1,27 +1,52 @@
 package rspasov.java8;
 
+import java.util.concurrent.Callable;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.function.Supplier;
 
 public class Concurrency {
 
+    private static String msg() {
+        return "Executing task in thread: " + Thread.currentThread().getName();
+    }
+
     public static void main(String[] args) throws Exception {
-        Runnable task = () -> {
-            System.out.println("Executing task in thread: " + Thread.currentThread().getName());
-            try {
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        };
+        ExecutorService executor = Executors.newFixedThreadPool(2);
+        try {
+            System.out.println(msg());
+            Runnable slowTask = () -> {
+                System.out.println(msg());
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            };
+            Thread newThread = new Thread(slowTask, "Custom Thread");
+            newThread.start();
 
-        ExecutorService executor = Executors.newSingleThreadExecutor();
+            executor.execute(slowTask);
+            executor.submit(slowTask).get(); // Returns null as Runnable.run() returns void.
 
-        Future<?> future = executor.submit(task);
-        System.out.println("Result is: " + future.get());
+            Callable<String> logMessageTask = () -> msg();
+            Future<String> future = executor.submit(logMessageTask);
+            System.out.println(future.get());
 
-        executor.shutdown();
+            CompletableFuture<Void> voidFuture = CompletableFuture.runAsync(slowTask, executor);
+            System.out.println(voidFuture.get());
+            // CompletableFuture.runAsync(logMessageTask, executor); // Does not compile.
+
+            Supplier<String> logMessageSupplier = () -> msg();
+            CompletableFuture<String> cFuture = CompletableFuture.supplyAsync(logMessageSupplier,
+                    executor);
+            System.out.println(cFuture.get());
+
+        } finally {
+            executor.shutdown();
+        }
     }
 
 }
