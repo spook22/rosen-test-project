10e9a1b7e960130198952ed83d6d9389af1f4f65
rspasov <rspasov@vmware.com>
2019-02-06 15:04:15 +0200

    .

--- java-tests/src/main/java/rspasov/java8/Concurrency.java
+++ java-tests/src/main/java/rspasov/java8/Concurrency.java
@@ -6,16 +6,28 @@ import java.util.concurrent.Executors;
 
 public class Concurrency {
 
+    private static void sleep(long millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
     private static String msg() {
         return "Executing task in thread: " + Thread.currentThread().getName();
     }
 
     private static void print(Object msg) {
         System.out.println(msg);
-        ;
     }
 
     public static void main(String[] args) throws Exception {
+        //        testExecutor();
+        testChaining();
+    }
+
+    protected static void testExecutor() throws Exception {
         ExecutorService executor = Executors.newFixedThreadPool(2);
         try {
             System.out.println(msg());
@@ -39,24 +51,75 @@ public class Concurrency {
         } finally {
             executor.shutdown();
         }
-
-        testChaining();
     }
 
-    private static void testChaining() {
+    protected static void testChaining() {
 
-        CompletableFuture<Void> vf =
-                CompletableFuture.runAsync(() -> {
-                    print("runAsync");
-                })
-                        .thenAccept(value -> {
-                            print("thenAccept " + value);
-                        });
+        //        CompletableFuture.runAsync(() -> {
+        //            print("runAsync " + msg());
+        //            sleep(1000);
+        //        })
+        //                .thenApply(value -> {
+        //                    print("thenApply (" + value + ") " + msg());
+        //                    sleep(500);
+        //                    return value;
+        //                })
+        //                .thenAccept(value -> {
+        //                    print("thenAccept (" + value + ") " + msg());
+        //                    sleep(500);
+        //                })
+        //                .thenRun(() -> {
+        //                    print("thenRun " + msg());
+        //                    sleep(500);
+        //                }).join();
 
-        vf.thenRun(() -> {
-            print("thenRun");
+        //        CompletableFuture.supplyAsync(() -> {
+        //            print("supplyAsync " + msg());
+        //            sleep(1000);
+        //            return msg();
+        //        })
+        //                .thenApply(value -> {
+        //                    print("thenApply (" + value + ") " + msg());
+        //                    return value;
+        //                })
+        //                .thenAccept(value -> {
+        //                    print("thenAccept (" + value + ") " + msg());
+        //                })
+        //                .thenRun(() -> {
+        //                    print("thenRun " + msg());
+        //                }).join();
+
+        CompletableFuture<?> future = CompletableFuture.supplyAsync(() -> {
+            sleep(1000);
+            print("supplyAsync " + msg());
+            return "supplyAsync";
         });
 
+        //        future.thenCompose(value -> {
+        //            print("thenCompose (" + value + ") " + msg());
+        //            return CompletableFuture.supplyAsync(() -> {
+        //                sleep(3000);
+        //                print("thenCompose CF (" + value + ") " + msg());
+        //                return "value";
+        //            });
+        //        });
+        future.thenAccept(value -> {
+            sleep(2000);
+            print("thenAcceptAsync (" + value + ") " + msg());
+        }).thenRunAsync(() -> {
+            sleep(3000);
+            print("thenRunAsync " + msg());
+        }).whenCompleteAsync((result, exc) -> {
+            sleep(2000);
+            print("whenComplete " + result + msg());
+        });
+
+
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
 }
